694963d654fce8f97dee16d16c24e1907a5b4a5c
wangjunyan <wangjunyan1984@gmail.com>
2015-03-18 18:42:56 +0800

    add some tree problems.
    Signed-off-by: wangjunyan <wangjunyan1984@gmail.com>

--- crack_code_interview/ch4/BinaryTree.java
+++ crack_code_interview/ch4/BinaryTree.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class BinaryTree{
 
@@ -123,6 +122,74 @@ public class BinaryTree{
         printTree(node.right);
     }
 
+    //traverse pre-order recursively
+    public void printPreOrder(){
+        printPreOrder(root);
+        System.out.println();
+    }
+
+    private void printPreOrder(Node node){
+        if(node != null){
+            System.out.println(node.data + " ");
+            printPreOrder(node.left);
+            printPreOrder(node.right);
+        }
+    }
+
+    //traverse pre-order iteratively
+    public void printPreOrderIter(){
+        Stack<Node> s = new Stack<Node>();
+        Node current = root;
+        do{
+            while(current != null){
+                System.out.print(current.data + " ");
+                s.push(current);
+                current = current.left;
+            }
+            current = s.pop().right;
+        }while(!s.isEmpty() || current!=null);
+        System.out.println();
+    }
+
+    //1) Create an empty stack nodeStack and push root node to stack.
+    //2) Do following while nodeStack is not empty.
+    //….a) Pop an item from stack and print it.
+    //….b) Push right child of popped item to stack
+    //….c) Push left child of popped item to stack
+    //Right child is pushed before left child to make sure that left subtree is processed first.
+    public void printPreOrderIter2(){
+        Stack<Node> s = new Stack<Node>();
+        Node current = root;
+        s.push(current);
+        while(!s.isEmpty()){
+            current = s.pop();
+            System.out.print(current.data + " ");
+            if(current.right!=null) s.push(current.right);
+            if(current.left!=null) s.push(current.left);
+        }
+        System.out.println();
+    }
+
+    //Excessive recursive function calls may cause memory to run out of stack space and extra overhead. 
+    //Since the depth of a balanced binary search tree is about lg(n), you might not worry about running out of stack space, 
+    //even when you have a million of elements. But what if the tree is not balanced? Then you are asking for trouble, 
+    //because in the worst case the height of the tree may go up to n. If that is the case, stack space will eventually 
+    //run out and your program will crash.
+    //Given a binary search tree, print the elements in-order iteratively without using recursion.
+    public void printInOrderIter(){
+        Stack<Node> s = new Stack<Node>();
+        Node current = root;
+        do{
+            while(current!=null){
+                s.push(current);
+                current = current.left;
+            }
+            current = s.pop();
+            System.out.print(current.data + " ");
+            current = current.right;
+        }while(!s.isEmpty() || current!=null);
+        System.out.println();
+    }
     /*
     Given a binary tree, print out the nodes of the tree according to a bottom-up "postorder" traversal 
     -- both subtrees of a node are printed out completely before the node itself is printed, 
@@ -381,72 +448,126 @@ public class BinaryTree{
     //Strategy: consider that each value could be the root. 
     //Recursively find the size of the left and right subtrees.
     public static int countTrees2(int numKeys){
-	if(numKeys <= 1) return 1;
-	else{
-	    //there will be one value at the root, with whatever remains
-   	    //on the left and right each forming their own subtrees. 
-	    //Iterate through all the values that could be the root...
-	    int sum = 0;
-	    int left, right, root;
-	    for(root = 1; root <= numKeys; root++){
-		left = countTrees2(root - 1);
-		right = countTrees2(numKeys-root);
-		// number of possible trees with this root == left*right 
-		sum += left*right;
-	    }
-	    return sum;
-	}
+        if(numKeys <= 1) return 1;
+        else{
+            //there will be one value at the root, with whatever remains
+            //on the left and right each forming their own subtrees. 
+            //Iterate through all the values that could be the root...
+            int sum = 0;
+            int left, right, root;
+            for(root = 1; root <= numKeys; root++){
+                left = countTrees2(root - 1);
+                right = countTrees2(numKeys-root);
+                // number of possible trees with this root == left*right 
+                sum += left*right;
+            }
+            return sum;
+        }
     }
 
     public static void countTrees(int numKeys){
-	int[] counts = new int[numKeys+1];
-	counts[0] = 1;
-	counts[1] = 1;
-	for(int i = 2; i <= numKeys; i++){
-		int count = 0;
-		for(int j = 0; j < i; j++){
-			count += counts[j]*counts[i-1-j];
-		}
-		counts[i] = count;
-		System.out.println("countTrees " + i + " : " + count);
-	}
+        int[] counts = new int[numKeys+1];
+        counts[0] = 1;
+        counts[1] = 1;
+        for(int i = 2; i <= numKeys; i++){
+            int count = 0;
+            for(int j = 0; j < i; j++){
+                count += counts[j]*counts[i-1-j];
+            }
+            counts[i] = count;
+            System.out.println("countTrees " + i + " : " + count);
+        }
     }
 
-
+    //Tests if a tree meets the conditions to be a binary search tree (BST). 
     public boolean isBST1(){
-	return isBST1(root);
+        return isBST1(root);
     }
 
+    //Recursive helper -- checks if a tree is a BST using minValue() and maxValue() (not efficient). 
     private boolean isBST1(Node node){
-	if(node == null) return true;
-	else if(node.left == null && node.right == null) return true;
-	else if(node.left != null && node.right == null) return ((node.data>=maxValue(node.left) && isBST1(node.left)));
-	else if(node.left == null && node.right != null) return ((node.data<=minValue(node.right) && isBST1(node.right)));
-	else return (node.data>=maxValue(node.left) && isBST1(node.left) && node.data<=minValue(node.right) && isBST1(node.right));
+        if(node == null) return true;
+        else if(node.left == null && node.right == null) return true;
+        else if(node.left != null && node.right == null) return ((node.data>=maxValue(node.left) && isBST1(node.left)));
+        else if(node.left == null && node.right != null) return ((node.data<=minValue(node.right) && isBST1(node.right)));
+        else return (node.data>=maxValue(node.left) && isBST1(node.left) && node.data<=minValue(node.right) && isBST1(node.right));
+    }
+    
+    public boolean isBST2(){
+        return isBST2(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
     }
 
+    //Efficient BST helper -- Given a node, and min and max values,
+    //recurs down the tree to verify that it is a BST, and that all
+    //its nodes are within the min..max range. Works in O(n) time -- 
+    //visits each node only once.
+    private boolean isBST2(Node node, int min, int max){
+        if(node == null) return true;
+        else{
+            if(node.data>=min && node.data<=max){
+                // left should be in range  min...node.data
+                // right should be in range node.data(+1)..max
+                return (isBST2(node.left, min, node.data) && isBST2(node.right, node.data, max));
+            }else{
+                return false;
+            }
+        }
+    }
+
+    //using Breadth First Search (BFS)
     public void printByLevel(){
         if(root == null) return;
-        LinkedQueue<Node> queue = new LinkedQueue<Node>();
-        queue.enqueue(root);
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(root);
         int num = 1;
         int newNum = 0;
         while(!queue.isEmpty()){
             for(int i = 0; i < num; i++){
-                Node node = queue.dequeue();
+                Node node = queue.remove();
                 System.out.print(node.data + " ");
                 if(node.left != null){
-                    queue.enqueue(node.left);
+                    queue.add(node.left);
                     newNum++;
                 }
                 if(node.right != null){
-                    queue.enqueue(node.right);
+                    queue.add(node.right);
                     newNum++;
                 }
             }
             System.out.println();
             num = newNum;
             newNum = 0;
+        }
+    }
+
+    //using Depth First Search (DFS)
+    /*
+    If you look carefully, you will notice that the DFS solution traverses the same node multiple times. 
+    Since BFS traverses each node exactly one time, BFS is much more efficient than DFS.
+    Although the DFS solution traverse the same node multiple times, it is not another order slower than the BFS solution. 
+    Here is the proof that the DFS solution above runs in O(N) time, where N is the number of nodes in the binary tree and 
+    we assume that the binary tree is balanced.
+    We first compute the complexity of printLevel for the kth level:
+    T(k) = 2T(k-1) + c = 2^(k-1)T(1) + c = 2^(k-1)
+    Assuming it’s a balanced binary tree, then it would have a total of lg N levels.
+    Therefore, the complexity of printing all levels is:
+    T(1) + T(2) + ... + T(lg N) = 1 + 2 + 2^2 + ... + 2^(lgN-1) + c = O(N)
+    Finding the maximum height of the tree also takes O(N) time, therefore the overall complexity is still O(N).
+    */
+    public void printByLevelDFS(){
+        int height = maxDepth();
+        for(int level = 1; level <= height; level++){
+            printByLevelDFS(root, level);
+            System.out.println();
+        }
+    }
+
+    private void printByLevelDFS(Node node, int level){
+        if(node == null) return;
+        if(level == 1) System.out.print(node.data + " ");
+        else{
+            printByLevelDFS(node.left, level-1);
+            printByLevelDFS(node.right, level-1);
         }
     }
 
@@ -504,32 +625,44 @@ public class BinaryTree{
         t4.printPaths();
         System.out.println("print by level:");
         t4.printByLevel();
+        System.out.println("print by level DFS:");
+        t4.printByLevelDFS();
         t4.printPretty2();
         t4.mirror();
         t4.printPretty2();
+        System.out.println("print t4 pre-order:");
+        t4.printPreOrder();
+        System.out.println("print t4 pre-order iteratively:");
+        t4.printPreOrderIter2();
+        System.out.println("print t4 in-order:");
+        t4.printTree();
+        System.out.println("print t4 in-order iteratively:");
+        t4.printInOrderIter();
 
-	countTrees(10);
-	for(int i = 1; i <=10; i++)
-	    System.out.println("countTrees2 " + i + " : " + countTrees2(i));
+        countTrees(10);
+        for(int i = 1; i <=10; i++)
+        System.out.println("countTrees2 " + i + " : " + countTrees2(i));
 	
-	initNodeArray();
-	BinaryTree t5 = new BinaryTree();
-	t5.root = nodeArray[5];
-	nodeArray[5].left = nodeArray[2];
-	nodeArray[5].right = nodeArray[7];
-	nodeArray[2].left = nodeArray[1];
-	nodeArray[2].right = nodeArray[6];
-	t5.printPretty2();
-	System.out.println("t5 is BST = " + t5.isBST1());
+        initNodeArray();
+        BinaryTree t5 = new BinaryTree();
+        t5.root = nodeArray[5];
+        nodeArray[5].left = nodeArray[2];
+        nodeArray[5].right = nodeArray[7];
+        nodeArray[2].left = nodeArray[1];
+        nodeArray[2].right = nodeArray[6];
+        t5.printPretty2();
+        System.out.println("t5 is BST = " + t5.isBST1());
+        System.out.println("t5 is BST2 = " + t5.isBST2());
 
-	initNodeArray();
-	BinaryTree t6 = new BinaryTree();
-	t6.root = nodeArray[5];
-	nodeArray[5].left = nodeArray[2];
-	nodeArray[5].right = nodeArray[7];
-	nodeArray[2].left = nodeArray[1];
-	t6.printPretty2();
-	System.out.println("t6 is BST = " + t6.isBST1());
+        initNodeArray();
+        BinaryTree t6 = new BinaryTree();
+        t6.root = nodeArray[5];
+        nodeArray[5].left = nodeArray[2];
+        nodeArray[5].right = nodeArray[7];
+        nodeArray[2].left = nodeArray[1];
+        t6.printPretty2();
+        System.out.println("t6 is BST = " + t6.isBST1());
+        System.out.println("t6 is BST2 = " + t6.isBST2());
     }
 
 
